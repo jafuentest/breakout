3f251c5a5a514cf2d2aeb3558cb44845f4fe2e9f
Juan Fuentes <jn.fuentest@gmail.com>
2019-07-31 23:06:15 -0400

    Destroy bricks when hit by ball <3

--- breakout.py
+++ breakout.py
@@ -13,6 +13,8 @@ BALL_SPEED = 0.75
 
 BRICK_SIZE = (40, 25)
 
+PUCK_SIZE = (170, 50)
+
 
 class Brick(object):
     """docstring for Brick"""
@@ -22,21 +24,29 @@ class Brick(object):
         self.j = j
 
     def draw(self, screen):
-        pygame.draw.rect(screen, GREY, self.getRect())
+        pygame.draw.rect(screen, WHITE, self.getRect())
+        pygame.draw.rect(screen, GREY, self.getInsideRect())
+
+    def getInsideRect(self):
+        return pygame.Rect(self.i + 2, self.j + 2, BRICK_SIZE[0] - 4, BRICK_SIZE[1] - 4)
 
     def getRect(self):
-        return pygame.Rect(self.i + 2, self.j + 2, BRICK_SIZE[0] - 4, BRICK_SIZE[1] - 4)
+        return pygame.Rect(self.i, self.j, BRICK_SIZE[0], BRICK_SIZE[1])
 
 
 class Ball(object):
     """docstring for Ball"""
     def __init__(self, puck, bricks):
         super(Ball, self).__init__()
+        self.bricks = bricks
         self.puck = puck
         self.direction = math.radians(270)
         self.speed = BALL_SPEED
         self.i = 400
         self.j = 300
+
+    def getRect(self):
+        return pygame.Rect((self.i, self.j), BALL_SIZE)
 
     def move(self):
         # If it hits a side of the screen, flip angle vertically
@@ -47,27 +57,38 @@ class Ball(object):
         if self.j < 0 or self.j > SCREEN_SIZE[1]:
             self.direction = math.radians(360 - math.degrees(self.direction))
 
-        was_hit_by_puck = self.getRect().colliderect(self.puck.getRect())
-        was_hit_with_the_top_of_puck = self.getRect().bottom - 1 <= self.puck.getRect().top
+        self.check_puck_collision()
+        self.check_bricks_collision()
+
+        # Set new position based on speed and angle
+        self.i += self.speed * math.cos(self.direction)
+        self.j -= self.speed * math.sin(self.direction)
+
+    def check_bricks_collision(self):
+        for brick in self.bricks:
+            did_hit_brick = self.getRect().colliderect(brick.getRect())
+
+            # If did hit a brick go up mirror direction horizontally
+            if did_hit_brick:
+                print (f"collision @ { self.i } { self.j }")
+                self.bricks.remove(brick)
+                self.direction = math.radians(360 - math.degrees(self.direction))
+
+    def check_puck_collision(self):
+        was_hit = self.getRect().colliderect(self.puck.getRect())
+        with_the_top = self.getRect().bottom - 1 <= self.puck.getRect().top
 
         # If hit by the puck go up and apply angle depending on which part of
         # the puck it was hit by
-        if was_hit_by_puck and was_hit_with_the_top_of_puck:
-            collision_x = self.i - self.puck.getX() + BALL_SIZE[0]
-            collision_x *= 150 / 180
+        if was_hit and with_the_top:
+            collision_x = self.i - self.puck.get_i() + BALL_SIZE[0]
+            collision_x *= PUCK_SIZE[0] / 180
             self.direction = math.radians(180 - collision_x)
-            print (f"mouse @ { collision_x }")
-
-        # Set new position base on speed and angle
-        self.i += self.speed * math.cos(self.direction)
-        self.j -= self.speed * math.sin(self.direction)
+            print (f"collision degrees: { collision_x }, new direction: { 180 - collision_x }")
 
     def draw(self, screen):
         self.move()
         pygame.draw.rect(screen, BLACK, self.getRect())
-
-    def getRect(self):
-        return pygame.Rect((self.i, self.j), BALL_SIZE)
 
 
 class Puck(object):
@@ -81,12 +102,12 @@ class Puck(object):
         pygame.draw.rect(screen, BLUE, self.getRect())
 
     def getRect(self):
-        return pygame.Rect(self.i, self.j, 170, 50)
+        return pygame.Rect(self.i, self.j, PUCK_SIZE[0], 50)
 
-    def getX(self):
+    def get_i(self):
         return self.i
 
-    def setX(self, i):
+    def set_i(self, i):
         self.i = i
 
 
@@ -127,7 +148,7 @@ def main():
             # React to the user moving the mouse
             if event.type == pygame.MOUSEMOTION:
                 mouse_position = pygame.mouse.get_pos()
-                puck.setX(mouse_position[0])
+                puck.set_i(mouse_position[0])
 
         pygame.display.update()
 
